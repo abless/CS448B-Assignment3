6ef34b2516995429047f2316844bc16251885f43
Alexander Blessing <abless@stanford.edu>
2011-10-25 21:38:51 -0700

    First try at stack chart - seems to work in principle

---
+++ sample.js
@@ -0,0 +1,6 @@
+var mortality = [
+  {"year":2000,"age":0,"sex":1, "stats": [{"cause": "Heart", "people":12}, {"cause": "Cancer", "people":14}, {"cause": "Other", "people": 10}]},
+  {"year":2000,"age":5,"sex":1, "stats": [{"cause": "Heart", "people":155}, {"cause": "Cancer", "people":240}]},
+  {"year":2000,"age":0,"sex":2, "stats": [{"cause": "Heart", "people":13}, {"cause": "Cancer", "people":143}, {"cause": "Other", "people": 10}]},
+  {"year":2000,"age":5,"sex":2, "stats": [{"cause": "Heart", "people":135}, {"cause": "Cancer", "people":240}]}
+];
